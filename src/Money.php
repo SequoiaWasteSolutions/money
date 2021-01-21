990f29e00b4a4f84ad6e9ea97d24d7e94405b04d
Travis Bennett <tandrewbennett@hotmail.com>
2021-01-21 16:58:08 -0600

    Remove validation logic checking that the sum of all ratios is non-negative (this is an unnecessary check and prevents valid use cases).

--- src/Money.php
+++ src/Money.php
@@ -352,10 +352,6 @@ final class Money implements \JsonSerializable
         $results = [];
         $total = array_sum($ratios);
 
-        if ($total <= 0) {
-            throw new \InvalidArgumentException('Cannot allocate to none, sum of ratios must be greater than zero');
-        }
-
         $nonNegativeRatios = array_filter($ratios, function($ratio) {
             return $ratio >= 0;
         });
